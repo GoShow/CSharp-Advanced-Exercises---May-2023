00553d356779f8104603b3f22a556e0366e724d1
Georgi Inkov <georgi.inkov@scaleforce.io>
2023-05-23 23:51:25 +0300

    Revert "Streams, Files and Directories solutions"
    This reverts commit c5853c683998ac0630f19acc3522cf4dd46e246d.

--- 05.FunctionalProgramming/01.ActionPrint/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-Action<string[]> print = (strings)
-    => Console.WriteLine(string.Join(Environment.NewLine, strings));
-
-string[] strings = Console.ReadLine()
-    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-print(strings);

ec5d0af675599546e03a038178962c8407a2fa61
Georgi Inkov <georgi.inkov@scaleforce.io>
2023-06-09 00:11:01 +0300

    Iterators and Comparators solutions

---
+++ 09.IteratorsAndComparators/00.Demos/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+ConcertHall concertHall = new(new List<int> { 1, 2, 3 });
+
+foreach (var seat in concertHall)
+{
+    Console.WriteLine(seat);
+}
+
+//ConcertHallEnumerator concertHallEnumerator = new(new List<int> { 1, 2, 3 });
+
+//while (concertHallEnumerator.MoveNext())
+//{
+//    Console.WriteLine(concertHallEnumerator.Current);
+//}
+
+
+public class ConcertHall : IEnumerable<int>
+{
+    private List<int> seats;
+
+    public ConcertHall(List<int> seats)
+    {
+        this.seats = seats;
+    }
+
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new ConcertHallEnumerator(seats);
+
+        //foreach (var seat in seats)
+        //{
+        //    yield return seat;
+        //}
+
+
+        //reversed
+        //for (int i = seats.Count - 1; i >= 0; i--)
+        //{
+        //    yield return seats[i];
+        //}
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public class ConcertHallEnumerator : IEnumerator<int>
+{
+    private int index = -1;
+    private List<int> seats;
+
+    public ConcertHallEnumerator(List<int> seats)
+    {
+        this.seats = seats;
+    }
+
+    public int Current => seats[index];
+
+    object IEnumerator.Current => Current;
+
+    public bool MoveNext()
+    {
+        index++;
+
+        return index < seats.Count;
+    }
+
+    public void Reset()
+    {
+        index = -1;
+    }
+
+    public void Dispose()
+    {
+    }
+}
---
+++ 09.IteratorsAndComparators/07.CustomComparator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Linq;
+
+Func<int, int, int> compare = (x, y) =>
+{
+    if (x % 2 == 0 && y % 2 != 0)
+    {
+        return -1;
+    }
+
+    if (x % 2 != 0 && y % 2 == 0)
+    {
+        return 1;
+    }
+
+    return x.CompareTo(y);
+};
+
+int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Array.Sort(numbers, (x, y) => compare(x, y));
+//Array.Sort(numbers, new CustomComparator());
+
+Console.WriteLine(string.Join(" ", numbers));
+
+//public class CustomComparator : IComparer<int>
+//{
+//    public int Compare(int x, int y)
+//    {
+//        if (x % 2 == 0 && y % 2 != 0)
+//        {
+//            return -1;
+//        }
+
+//        if (x % 2 != 0 && y % 2 == 0)
+//        {
+//            return 1;
+//        }
+
+//        return x.CompareTo(y);
+//    }
+//}
